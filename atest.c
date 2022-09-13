d1f8c05c6452619d2b31776b5aa94b2c9e620db8
wwhai <cnwwhai@gmail.com>
2022-09-13 18:13:43 +0800

    dev: add test cases

--- atest.c
+++ atest.c
@@ -1,47 +1,116 @@
-#include <stdio.h>
 #include "iothub.h"
-#include "atest.h"
 #include "iothubschema.h"
 #include <stdlib.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+int publish();
 /**
- * 单元测试
- **/
+ * 消息回调
+ *
+ */
+
+PRIVATE void OnProperty(struct iothubsdk *sdk, iothub_down_msg msg)
+{
+    iothub_reply_msg rMsg = {
+        .method = "property_reply",
+        .id = msg.id,
+        .code = 10086,
+        .timestamp = 123456,
+        .status = "TEST-OK",
+    };
+    SDKPropertyReply(sdk, rMsg);
+};
+PRIVATE void OnAction(struct iothubsdk *sdk, iothub_down_msg msg)
+{
+    iothub_reply_msg rMsg = {
+        .method = "action_reply",
+        .id = msg.id,
+        .code = 10086,
+        .timestamp = 123456,
+        .actionid = "test",
+        .status = "test",
+        .out = {
+            .action_p1 = 1001,
+            .action_p2 = 2001,
+        }};
+    SDKActionReply(sdk, rMsg);
+};
+/**
+ * 未识别消息回调
+ *
+ */
+PRIVATE void OnMessage(struct iothubsdk *sdk, char *message){
+
+};
+/**
+ * 掉线回调
+ *
+ */
+PRIVATE void OnClosed(struct iothubsdk *sdk, char *reason){
+
+};
+/**
+ * 消息发布成功回调
+ *
+ */
+PRIVATE void OnDeliver(struct iothubsdk *sdk, int token){
+
+};
+// 测试发送消息
+
+int publish()
+{
+    MQTTClient client;
+    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+    conn_opts.username = "test-sender";
+    conn_opts.password = "test-sender";
+    conn_opts.cleansession = 1;  // True
+    conn_opts.retryInterval = 5; // 5s
+    int rc;
+    if ((rc = MQTTClient_create(&client, MQTT_HOST, "test-sender",
+                                MQTTCLIENT_PERSISTENCE_NONE, NULL)) != MQTTCLIENT_SUCCESS)
+    {
+        log_error("failed to create sdk, return code %d", rc);
+        return 0;
+    }
+    if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS)
+    {
+        log_error("failed to connect mqtt server: %s, username: %s, clientid: %s, return code %d",
+                  MQTT_U, MQTT_C, MQTT_HOST, rc);
+        return rc; // -1
+    }
+    const char *jsons1 = "{\"method\":\"action\",\"id\":\"test\",\"actionid\":\"test\",\"timestamp\":159,\"data\":{\"a\":147,\"b\":258}}";
+    const char *jsons2 = "{\"method\":\"property\",\"id\":\"test\",\"actionid\":\"test\",\"timestamp\":159,\"data\":{\"a\":147,\"b\":258}}";
+    MQTTClient_publish(client, "$thing/down/property/test/test1", strlen(jsons1), jsons1, 0, 0, NULL);
+    MQTTClient_publish(client, "$thing/down/action/test/test1", strlen(jsons2), jsons2, 0, 0, NULL);
+}
+/**
+ *
+ *
+ */
 int main(int argc, char const *argv[])
 {
 
     log_set_level(LOG_DEBUG);
-    iothub_property p = {
-        1,
-        2,
-    };
-    SDKBuildPropertyMsg(p);
-    iothub_reply_msg m = {
-        .method = "property_reply",
-        .id = "id00000001",
-        .code = 1,
-        .timestamp = 1008,
-        .status = "OK",
-    };
-    SDKBuildPropertyReplyMsg(m);
-    //
-    iothub_down_msg msg;
-    //{\"method\":\"property\",\"id\":\"1662828896480999\",\"timestamp\":1662828896480999,\"data\":{\"a\":1}
-    SDKParseDownMsg(&msg, "{\"method\":\"action\",\"id\":\"abcde\",\"actionid\":\"actionid-1\",\"timestamp\":199,\"data\":{\"a\":1,\"b\":2}}");
-    printf("SDKParseDownMsg TEST==> msg.id: %s, msg.method: %s, msg.actionid: %s, msg.timestamp: %ld, msg.p.a: %d, msg.p.b: %d\n",
-           msg.id, msg.method,msg.actionid, msg.timestamp, msg.property_data.a, msg.property_data.b);
-    //
-    iothub_reply_msg am = {
-        .method = "action_reply",
-        .id = "id00000001",
-        .code = 1,
-        .timestamp = 1,
-        .status = "OK",
-        .actionid = "id00000001",
-        .out = {
-            .action_p1 = 1,
-            .action_p2 = 2,
-        },
-    };
-    SDKBuildPropertyReplyMsg(am);
+    struct iothubsdk *sdk = SDKNewMqttDevice();
+    if (sdk == NULL)
+    {
+        log_fatal("sdk create failed");
+        return 0;
+    }
+    if (SDKSetCallback(sdk, OnMessage, OnClosed, OnDeliver, OnProperty, OnAction) != 0)
+    {
+        log_fatal("callback create failed");
+        return 0;
+    }
+    if (SDKStart(sdk) != 0)
+    {
+        log_fatal("sdk start failed");
+        return 0;
+    }
+    publish();
+    sleep(2);
+    SDKStop(sdk);
     return 0;
 }
--- iothubconfig.h
+++ iothubconfig.h
@@ -5,9 +5,9 @@
 // SDK默认配置模板
 //--------------------------------------------------------------------------------------------------
 #define MQTT_HOST "tcp://127.0.0.1:1883"
-#define MQTT_C "RULEX-001"
-#define MQTT_U "RULEX-001"
-#define MQTT_P "RULEX-001"
-#define IOTHUB_PRODUCT_ID "RULEX"
+#define MQTT_C "test1"
+#define MQTT_U "test1"
+#define MQTT_P "test1"
+#define IOTHUB_PRODUCT_ID "test"
 
 #endif
